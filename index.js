21b85a8564167049b24543b0423be0cb93293553
Mmunni <masumamunni20@gmail.com>
2022-10-25 18:52:25 -0700

    category json loaded

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
   res.send('Courses Api Running')
@@ -15,12 +16,26 @@ app.get('/courses-categories', (req, res) => {
   res.send(categories)
 });
 
-// app.get('/category/:id', (req, res) => {
-//   const id = req.params.id;
-//   if(id === '06'){
-//     res.send(news);
-//   }
-// })
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if(id === '06'){
+    res.send(courses);
+  }
+  else{
+    const category_courses = courses.filter( c => c.category_id === id);
+     res.send(category_courses)
+  }
+})
+
+app.get('/courses', (req, res) => {
+  res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = courses.find( c => c._id === id);
+  res.send(selectedCourses)
+})
 
 app.listen(port, () => {
   console.log(`Etrain Courses Server ${port}`)
